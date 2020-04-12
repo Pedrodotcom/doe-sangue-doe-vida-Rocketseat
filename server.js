3b68489921fa1a20b8228a200945e5340dea1137
Pedrodotcom <pedroluisaantonio@gmail.com>
2020-04-12 13:26:34 -0300

    Constituição do banco de dados.

--- server.js
+++ server.js
@@ -5,4 +5,75 @@ const db = require('./db')
 
 server.use(express.static("public"))
 
+server.use(express.urlencoded({ extended: true }))
+
+const nunjucks = require("nunjucks")
+    nunjucks.configure("views", {
+        express: server,
+        noCache: true,
+    })
+
+server.get("/", function(request, response) {
+
+    db.all(`SELECT * FROM cadastro`, function(err, rows) {
+        if (err) {
+            return response.send(`Erro no banco de dados. Por favor, tente novamente`)
+        }
+
+        const reverseDonors = rows.reverse()
+
+        let lastDonors = []
+
+        for (let donor of reverseDonors) {
+            if (lastDonors.length < 4) {
+                lastDonors.push(donor)
+            }
+        }
+
+        return response.render("index.html", { cadastro: lastDonors })
+    })
+
+})
+
+server.get("/", function(request, response) {
+
+    db.all(`SELECT * FROM cadastro`, function(err, rows) {
+        if (err) {
+            return response.send(`Erro no banco de dados. Por favor, tente novamente.`)
+        }
+
+        const reverseDonors = rows.reverse()
+
+        return response.render(`index.html`, { cadastro: reverseDonors })
+    })
+
+})
+
+server.post("/", function(request, response) {
+
+    const query = `
+        INSERT INTO cadastro(
+            name,
+            email,
+            bloodtype
+        ) VALUES (?, ?, ?);
+    `
+
+    const values = [
+        request.body.name,
+        request.body.email,
+        request.body.bloodtype
+    ]
+
+    db.run(query, values, function(err) {
+        if (err) {
+            return response.send(`Erro no banco de dados. Por favor, tente novamente.`)
+        }
+
+        return response
+            .redirect("/")
+            .alert(`Cadastro concluído com sucesso. Obrigado por se comprometer!`)
+    })
+})
+
 server.listen(3000)
